a918fd3fedfe1acc9a87ff2d4b5885e69fb93106
zhufengzhufeng <894918097@qq.com>
2017-07-19 13:04:37 +0800

    todo

---
+++ 002/todo.js
@@ -0,0 +1,10 @@
+new Vue({
+    el:'#app',
+    data:{
+      todos:[
+          {isSelected:true,title:'晚上回家睡觉'},
+          {isSelected:true,title:'晚上回家睡觉'},
+          {isSelected:true,title:'晚上回家睡觉'}
+      ]
+    }
+});
