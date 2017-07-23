16e69d0c5beefc19f52dc0828f4201219aa00253
zhufengzhufeng <894918097@qq.com>
2017-07-23 17:27:17 +0800

    剩余运算符 展开预算符/拓展运算符

--- node/6.events/1.events.js
+++ node/6.events/1.events.js
@@ -8,14 +8,19 @@ Girl.prototype.on = function (eventName,callback) { //订阅
         this._events[eventName] = [callback];
     }
 };
-Girl.prototype.emit = function (eventName) { //发布
+
+Girl.prototype.emit = function (eventName,...arg) { //发布  剩余运算符
+    //console.log(...arg); //展开运算符
+    //[].slice.call(arguments,1)
+    //Array.from(arguments)  将类数组转换成数组
     if(this._events[eventName]){
-        this._events[eventName].forEach(item=>item());
+        //item 代表的是cry 和eat函数   这里的call的this代表的是girl的实例，不call就是global
+        this._events[eventName].forEach(item=>item.call(this,...arg));
     }
 };
 let girl = new Girl();
-function cry() {console.log('大哭');}
-function eat() {console.log('吃');}
+function cry(who) {console.log(who+'大哭',this);}
+function eat(who) {console.log(who+'吃');}
 girl.on('女生失恋',cry); //cry eat都是异步方法，只有触发emit的时候才会执行
 girl.on('女生失恋',eat);
-girl.emit('女生失恋');
+girl.emit('女生失恋','你','a','b');
