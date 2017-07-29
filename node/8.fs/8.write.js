9ef9b9bcaeaa865a55f4abcde3a0aea2c5885475
zhufengzhufeng <894918097@qq.com>
2017-07-29 16:27:01 +0800

    readFile writeFile

--- node/8.fs/8.write.js
+++ node/8.fs/8.write.js
@@ -3,7 +3,7 @@
 let fs = require('fs');
 // 写的特点  1.写入的编码格式 是字符串 utf8格式,会将内容toString
 //         2.写的文件不存在 如果文件不存在则会创建，如果存在会清空内容，在写入
-//fs.writeFileSync('1.txt',2);
+//fs.writeFileSync('1.txt',new Buffer('珠峰'));
 
 //异步写入的方法
 fs.writeFile('1.txt',3,function (err) { //成功后的回调
--- node/8.fs/9.copy.js
+++ node/8.fs/9.copy.js
@@ -1,12 +1,21 @@
 //读出source的内容 写入到target中
+let fs = require('fs');
+/*fs.readFileSync('1.txt','utf8');
+fs.readFile('1.txt','utf8',callback);
+fs.writeFileSync('2.txt','xxx');
+fs.writeFile('2.txt','xxx',callback);*/
 function copySync(source,target) { //readFileSync writeFileSync
-
+    let result = fs.readFileSync(source);
+    fs.writeFileSync(target,result)
 }
-
 function copy(source,target,callback) { //readFile writeFile
-
+    fs.readFile(source,function (err,data) {
+        if(err) return callback(err); //如果有错误调用callback传递错误
+        fs.writeFile(target,data,callback)
+    })
 }
 //copySync('1.txt','2.txt');
-copy('1.txt','2.txt',function () {
+copy('1.txt','2.txt',function (err) {
+    if(err) return console.log(err);
     console.log('拷贝成功');
 });
