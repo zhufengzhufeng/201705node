2d9451c3dcf60a8135ccb7564250c0395b05d46e
zhufengzhufeng <894918097@qq.com>
2017-07-30 15:18:31 +0800

    manager

---
+++ node/10.http/manager/server.js
@@ -0,0 +1,22 @@
+let http = require('http');
+let fs = require('fs');
+let mime = require('mime');
+let url = require('url');
+http.createServer(function (req,res) {
+    let {pathname,query} = url.parse(req.url,true);
+    if(pathname === '/'){
+        res.setHeader('Content-Type','text/html;charset=utf8');
+        fs.createReadStream('index.html').pipe(res);
+    }
+
+    else{
+        fs.exists('.'+pathname,function (flag) {
+            if(flag){
+                res.setHeader('Content-Type',mime.lookup(pathname)+';charset=utf8');
+                fs.createReadStream('.'+pathname).pipe(res);
+            }else{
+                res.statusCode = 404;res.end();
+            }
+        })
+    }
+}).listen(8080);
