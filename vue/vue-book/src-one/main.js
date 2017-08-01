c11a9bdad6d9374d4c19eedab9ef0b4505df846e
zhufengzhufeng <894918097@qq.com>
2017-08-01 11:57:52 +0800

    App

--- vue/vue-book/src-one/main.js
+++ vue/vue-book/src-one/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import './common/index.less';
-new Vue({
-  el:'#app',
-  router,
-  render:h=>h(App)
+import Vue from 'vue'; //引入vue
+import App from './App.vue'; //引入根组件
+import router from './router'; //引入路由
+import './common/index.less'; //公共样式
+new Vue({ //创建一个实例
+  el:'#app', //挂载点
+  router, //路由实例
+  render:h=>h(App) //默认渲染App组件
 });
